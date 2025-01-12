ff3234607f4687ca366e6d0111a97fb43e046c70
Gregory Bowne <gbowne144@gmail.com>
2025-01-11 19:42:44 -0800

    Create filesystem.go
    Add file system work

---
+++ filesystem.go
@@ -0,0 +1,35 @@
+package filesystem
+
+import (
+	"path/filepath"
+	"sort"
+)
+
+type FilesystemEntry struct {
+	Path string
+	Size int64
+}
+
+type Filesystem struct {
+	Entries []FilesystemEntry
+}
+
+func WalkDirectory(dir string) ([]FilesystemEntry, error) {
+	entries := []FilesystemEntry{}
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			entries = append(entries, FilesystemEntry{Path: path, Size: info.Size()})
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	sort.Slice(entries, func(i, j int) bool {
+		return entries[i].Size > entries[j].Size
+	})
+	return entries, nil
+}
